ebe36e398f62d4393be732cb3ad8698748d58612
mradke2 <mrbunited@gmail.com>
2018-10-11 19:42:56 -0500

    Updates

---
+++ precache-manifest.d7227b974c0f6933f61d2481ab7ef319.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "85227ac9ee6bfb9063e7",
+    "url": "/Clicky-Game/static/js/main.85227ac9.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/Clicky-Game/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "85227ac9ee6bfb9063e7",
+    "url": "/Clicky-Game/static/css/main.9684e7e8.chunk.css"
+  },
+  {
+    "revision": "0a45bb63169332940cd5120b058cad01",
+    "url": "/Clicky-Game/index.html"
+  }
+];
